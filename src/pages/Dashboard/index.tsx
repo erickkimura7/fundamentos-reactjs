81bf4e8dd8a90d67c782ffd230d522c72a64f311
Erick Kimura <erick_k_x@hotmail.com>
2020-12-02 02:17:42 -0300

    Ajustes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -100,7 +100,7 @@ const Dashboard: React.FC = () => {
               {transactions.map(transaction => (
                 <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
-                  <td className={transaction.type}>R$ {transaction.formattedValue}</td>
+                  <td className={transaction.type}>{transaction.type === 'outcome' && '-'} {transaction.formattedValue}</td>
                   <td>{transaction.category && transaction.category.title}</td>
                   <td>{transaction.formattedDate}</td>
                 </tr>
